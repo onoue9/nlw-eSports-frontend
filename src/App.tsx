3664eb7d44d552074473cef7ff00f6b35b36a9cf
Filipe Onoue <filipeflom@gmail.com>
2022-09-30 11:54:07 -0300

    refactor: :recycle: change the App to a BrowserRouter controller

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,20 @@
 import './styles/main.css';
+import { BrowserRouter, Route, Routes, useRoutes } from 'react-router-dom';
+import routes from '~react-pages'
+import { Suspense } from 'react'
+import { Home } from './components/Home/Home';
+import { Game } from './pages/Game/Game';
 
-import logoImg from './assets/logo-nlw-esports.svg';
-import { GameBanner } from './components/GameBanner';
-import { CreateAdBanner } from './components/CreateAdBanner';
-import { useEffect, useState } from 'react';
-import * as Dialog from '@radix-ui/react-dialog'
-import { GameController } from 'phosphor-react';
-import { Input } from './components/Form/input';
-import { CreateAdModal } from './components/CreateAdModal';
-import axios from 'axios';
 
-interface Game {
-  id: string;
-  title: string;
-  bannerUrl: string;
-  _count: {
-    ads: number;
-  }
-}
 
 function App() {
-  const [games, setGames] = useState<Game[]>([]);
-
-  useEffect(() => {
-    axios('https://nlw-esports-backend-onoue9.herokuapp.com/games')
-      .then(response => setGames(response.data));
-  }, []);
-
   return (
-    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
-      <img src={logoImg} alt="" />
-
-      <h1 className="text-6xl text-white font-black mt-20">
-        Seu <span className="text-transparent bg-nlw-gradient bg-clip-text">duo</span> está aqui
-      </h1>
-
-      <div className="grid grid-cols-6 gap-6 mt-16">
-        {games.map(game => (
-          <GameBanner
-            key={game.id}
-            bannerUrl={game.bannerUrl}
-            title={game.title}
-            adsCount={game._count.ads}
-          />
-        ))}
-      </div>
-
-      <Dialog.Root>
-        <CreateAdBanner />
-
-        <CreateAdModal />
-      </Dialog.Root>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/nlw-esports-frontend/" element={<Home />} />
+        <Route path="/nlw-esports-frontend/game/:gameName" element={<Game />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
