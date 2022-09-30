35c99f5102132dfd90dd42dedddf2d33fb54f99e
Filipe Onoue <filipeflom@gmail.com>
2022-09-30 11:56:18 -0300

    refactor: :recycle: change ancor to NavLink from react-router-dom

--- src/components/GameBanner.tsx
+++ src/components/GameBanner.tsx
@@ -1,3 +1,5 @@
+import { NavLink } from 'react-router-dom';
+
 interface GameBannerProps {
   bannerUrl: string;
   title: string;
@@ -6,13 +8,16 @@ interface GameBannerProps {
 
 export function GameBanner(props: GameBannerProps) {
   return (
-    <a href="" className="relative rounded-lg overflow-hidden">
+    <NavLink
+      to={`./game/${props.title}`}
+      className="relative rounded-lg overflow-hidden"
+    >
       <img src={props.bannerUrl} alt="" />
 
       <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
         <strong className="font-bold text-white block">{props.title}</strong>
         <span className="text-zinc-300 text-sm block">{props.adsCount} anúncio(s)</span>
       </div>
-    </a>
+    </NavLink>
   )
 }
