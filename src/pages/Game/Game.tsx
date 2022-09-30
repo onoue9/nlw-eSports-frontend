92e60b32457b2545830f7e8da3d717d1b9fd6cc5
Filipe Onoue <filipeflom@gmail.com>
2022-09-30 11:56:43 -0300

    feat: :sparkles: implemented Game page

---
+++ src/pages/Game/Game.tsx
@@ -0,0 +1,93 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+import { NavLink } from 'react-router-dom';
+import logoImg from '../../assets/logo-nlw-esports.svg';
+import { DuoBanner } from '../../components/DuoBanner';
+
+interface Game {
+  id: string;
+  title: string;
+  bannerUrl: string;
+  _count: {
+    ads: number;
+  }
+}
+
+interface Duo {
+  id: string;
+  name: string;
+  yearsPlaying: number;
+  hourStart: string;
+  hourEnd: string;
+  weekDays: string[];
+  useVoiceChannel: boolean;
+}
+
+export function Game() {
+  const { gameName } = useParams();
+  const [duos, setDuos] = useState<Duo[]>([]);
+  const [game, setGame] = useState<Game>();
+
+  useEffect(() => {
+    axios('https://nlw-esports-backend-onoue9.herokuapp.com/games')
+      .then(response => setGame(
+        response.data.find(
+          (game: Game) => game.title === gameName
+        )))
+  }, []);
+
+  useEffect(() => {
+    const id = game?.id;
+    console.log(id)
+    axios(`https://nlw-esports-backend-onoue9.herokuapp.com/games/${id}/ads`)
+      .then(response => setDuos(response.data))
+  }, [game])
+
+  
+  
+  return (
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+      <NavLink to="/nlw-esports-frontend/">
+        <img src={logoImg} alt="" />
+      </NavLink>
+
+      <div className="mt-20">
+        <div className="w-auto">
+          <img
+            className=""
+            src={game?.bannerUrl}
+            alt="Game banner"
+          />
+        </div>
+        <div className="my-10">
+          <strong className="text-2xl text-white font-black bloack">
+            {gameName}
+          </strong>
+          <span className="text-zinc-400 block">
+            Conecte-se e comece a jogar!
+          </span>
+        </div>
+      </div>
+
+      <div className="flex flex-row">
+        {duos.length > 0 ? duos.map(duo => (
+          <DuoBanner 
+            key={duo.id}
+            id={duo.id}
+            name={duo.name}
+            yearsPlaying={duo.yearsPlaying}
+            hourStart={duo.hourStart}
+            hourEnd={duo.hourEnd}
+            weekDays={duo.weekDays}
+            useVoiceChannel={duo.useVoiceChannel}
+            bannerUrl={game?.bannerUrl}
+            title={gameName}
+            adsCount={game?._count.ads}
+          />
+        )) : <p className="text-white">Loading...</p>
+      }
+      </div>
+    </div>
+  )
+}
